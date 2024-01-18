577a49de0e9785565ccda8c243bd485ebf5bf4d1
dangorogoro <dangorogoro0802@gmail.com>
2024-01-18 23:26:59 +0900

    Add antialiasing

--- InOneWeekend/primitives/camera.go
+++ InOneWeekend/primitives/camera.go
@@ -3,11 +3,13 @@ package primitives
 import (
   "fmt"
   "os"
+  "math/rand"
 )
 type Camera struct {
   lowerLeft, horizontal, vertical, origin, pixel00_loc, pixel_delta_u, pixel_delta_v, center Vec3
   image_width, image_height uint32
   aspect_ratio float64
+  samples_per_pixel int32
 }
 
 func NewCamera(aspect_ratio float64, image_width uint32) *Camera {
@@ -37,32 +39,33 @@ func (c *Camera) position(u float64, v float64) Vec3 {
 func (c *Camera) getDirection(position Vec3) Vec3 {
   return c.lowerLeft.Add(position)
 }
-func (c *Camera) Rendar(world Hittable) {
+func (c *Camera) Rendar(world Hittable, alias_sampling int32) {
   fmt.Fprintln(os.Stderr, "\rRendar start")
-  c.initialize()
+  c.initialize(alias_sampling)
   fmt.Printf("P3\n%d %d\n255\n", c.image_width, c.image_height)
   for j := int32(c.image_height - 1); j >= 0; j-- {
     fmt.Fprintln(os.Stderr, "\rScanlines remaining:", j)
-    for i := uint32(0); i < c.image_width; i++ {
-      var pixel_center = c.pixel00_loc.Add(c.pixel_delta_u.MulScalar(float64(i))).Add(c.pixel_delta_v.MulScalar(float64(j)))
-      var ray_direction = pixel_center.Sub(c.center)
-      var r = Ray{c.center, ray_direction}
-
-      pixel_color := Ray_color(&r, world)
-      Write_color(pixel_color)
+    for i := int32(0); i < int32(c.image_width); i++ {
+      var pixel_color = Vec3{0, 0, 0}
+      for sample := int32(0); sample < c.samples_per_pixel; sample++ {
+        var r = c.getRay(i, j)
+        pixel_color = pixel_color.Add(Ray_color(&r, world))
+      }
+      Write_color(pixel_color, c.samples_per_pixel)
     }
   }
   fmt.Fprintln(os.Stderr, "\nDone.\n")
 
 }
-func (c *Camera) initialize() {
+func (c *Camera) initialize(alias_sampling int32) {
   if c.image_height < 1 {
     c.image_height = 1
   }
+  c.samples_per_pixel = alias_sampling
   var center = Vec3{0, 0, 0}
   // Determine viewport dimensions
   var focal_length = 1.0
-  var viewport_height = 2.0;
+  var viewport_height = 2.0
   var viewport_width = viewport_height * c.aspect_ratio
   
   // Calculate the vectors across the horizontal and down the vertical viewport edges.
@@ -81,4 +84,17 @@ func (c *Camera) initialize() {
 
 func (c *Camera) rayColor(r *Ray, world Hittable) Vec3 {
   return Ray_color(r, world)
+}
+
+func (c *Camera) getRay(i int32, j int32) Ray {
+  var pixel_center = c.pixel00_loc.Add(c.pixel_delta_u.MulScalar(float64(i))).Add(c.pixel_delta_v.MulScalar(float64(j)))
+  var pixel_sample = pixel_center.Add(c.pixelSampleSquare())
+  var ray_direction = pixel_sample.Sub(c.center)
+  return Ray{c.center, ray_direction}
+}
+
+func (c *Camera) pixelSampleSquare() Vec3 {
+  var px = -0.5 + rand.Float64()
+  var py = -0.5 + rand.Float64()
+  return c.pixel_delta_u.MulScalar(px).Add(c.pixel_delta_v.MulScalar(py))
 }
--- InOneWeekend/primitives/color.go
+++ InOneWeekend/primitives/color.go
@@ -4,6 +4,7 @@ import (
   "fmt"
 )
 
-func Write_color(c Vec3) {
-  fmt.Printf("%d %d %d\n", uint8(c.X * 255.999), uint8(c.Y * 255.999), uint8(c.Z * 255.999))
+func Write_color(c Vec3, samples_per_pixel int32) {
+  var avg_color = c.DivScalar(float64(samples_per_pixel))
+  fmt.Printf("%d %d %d\n", uint8(avg_color.X * 255.999), uint8(avg_color.Y * 255.999), uint8(avg_color.Z * 255.999))
 }
