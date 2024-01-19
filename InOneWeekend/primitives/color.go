14925c66f37c681d6538b3d5ecfbcfe732757622
dangorogoro <dangorogoro0802@gmail.com>
2024-01-19 23:56:35 +0900

    Apply gamma correction

--- InOneWeekend/primitives/color.go
+++ InOneWeekend/primitives/color.go
@@ -2,9 +2,14 @@ package primitives
 
 import (
   "fmt"
+  "math"
 )
 
 func Write_color(c Vec3, samples_per_pixel int32) {
   var avg_color = c.DivScalar(float64(samples_per_pixel))
-  fmt.Printf("%d %d %d\n", uint8(avg_color.X * 255.999), uint8(avg_color.Y * 255.999), uint8(avg_color.Z * 255.999))
+  // Apply the linear to gamma transform.
+  var gamma_filtered_r = math.Sqrt(avg_color.X)
+  var gamma_filtered_g = math.Sqrt(avg_color.Y)
+  var gamma_filtered_b = math.Sqrt(avg_color.Z)
+  fmt.Printf("%d %d %d\n", uint8(gamma_filtered_r * 255.999), uint8(gamma_filtered_g * 255.999), uint8(gamma_filtered_b * 255.999))
 }
